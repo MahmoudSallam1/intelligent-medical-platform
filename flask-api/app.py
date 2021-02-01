acb2c858dc2421fb58179a350ca9ba0dad3a0832
sallam <diabound2@outlook.com>
2021-02-01 15:28:57 +0200

    adding home , signin , signup page

--- flask-api/app.py
+++ flask-api/app.py
@@ -13,13 +13,3 @@ def say_hello_world():
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-# for number in range(1, 55):
-#     if number % 2 == 0:
-#         print(number + " is divisable by 2")
-#     elif number % 3 == 0:
-#         print(number + " is divisable by 3")
-#     elif (number % 2 == 0 and number % 3 == 0):
-#         print(number + "is divisable by 2 and 3")
-#     else:
-#         print(number + " is  NOT divisable by 2 or 3")
