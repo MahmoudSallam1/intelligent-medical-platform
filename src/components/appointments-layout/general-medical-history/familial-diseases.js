1cb16a722ccf14b3458607567a550c66519c2b54
Yuumna <yumnaali16@gmail.com>
2021-03-23 00:16:21 +0200

    adding familial diseases

--- src/components/appointments-layout/general-medical-history/familial-diseases.js
+++ src/components/appointments-layout/general-medical-history/familial-diseases.js
@@ -1,10 +1,116 @@
 import React from 'react'
+import OurGrid from "../../grid/grid";
+import Grid from "@material-ui/core/Grid";
+
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    overflow: "auto",
+    flexDirection: "column",
+  },
+  breath: {
+    marginTop: "1em",
+    textAlign:"left"
+  },
+
+  
+  ourGrid:{
+  }
+
+}));
 
 function FamilialDiseases() {
+
+    const classes = useStyles();
+
+    const [state, setState] = React.useState({
+        checkedA: true,
+        checkedB: true,
+       
+      });
+    
+      const handleChange = (event) => {
+        setState({ ...state, [event.target.name]: event.target.checked });
+      };
     return (
-        <div>
-            this is Familial diseases
-        </div>
+        <OurGrid className={classes.ourGrid}>
+        <Grid item xs={12} md={12} lg={12}>
+          <Typography
+            
+            align={"left"}
+            variant="h7"
+            bold
+            gutterBottom
+          >
+            Check those to which the answer is yes(leave other blank).
+          </Typography>
+          <br/>
+          <FormControlLabel
+          margin="normal"
+        control={
+          <Checkbox
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"
+            color="primary"
+            align={"left"}
+          />
+        }
+        label="Heart attack under age 50"
+      />
+                <FormControlLabel
+                margin="normal"
+        control={
+          <Checkbox
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"
+            color="primary"
+            align={"left"}
+          />
+        }
+        label="Heart attack under age 50"
+      />
+                <FormControlLabel
+                margin="normal"
+        control={
+          <Checkbox
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"
+            color="primary"
+            align={"left"}
+            
+          />
+        }
+        label="Heart attack under age 50"
+      />
+                <FormControlLabel
+                margin="normal"
+        control={
+          <Checkbox
+            checked={state.checkedB}
+            onChange={handleChange}
+            name="checkedB"
+            color="primary"
+            align={"left"}
+            margin="normal"
+          />
+        }
+        label="Heart attack under age 50"
+      />
+     
+
+
+     </Grid>
+        
+      </OurGrid>
     )
 }
 
