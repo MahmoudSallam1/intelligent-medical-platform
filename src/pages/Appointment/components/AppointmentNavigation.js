05da66799bb8c7d1457a488e0852bdc5fe6e74a7
MahmoudSallam1 <sallam@msallam.me>
2022-07-26 09:21:53 +0200

    Removed appointment navigation from patient medical data

--- src/pages/Appointment/components/AppointmentNavigation.js
+++ src/pages/Appointment/components/AppointmentNavigation.js
@@ -70,9 +70,9 @@ function AppointmentNavigation() {
     <div className={classes.root}>
       <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="simple tabs example">
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="General Information" {...a11yProps(0)} />
+          <Tab label="Medical Data" {...a11yProps(1)} />
+          <Tab label="Intelligent Prescription" {...a11yProps(2)} />
         </Tabs>
       </AppBar>
       <TabPanel value={value} index={0}>
